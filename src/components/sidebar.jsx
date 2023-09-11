feaad25c2c5ffed271361d3a86a9d0f71ce7f5da
user18718722 <vortexxd5@gmail.com>
2023-09-11 19:55:43 +0800

    update

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link, useLocation, useParams } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
 export default function Sidebar() {
-    const accountID = useParams()
     const [view, setView] = useState()
-    const location = useLocation()
 
     const getData = async (response) => {
         try {
 
-            const { data } = await axios.get(`http://localhost:8080/account/${accountID}`)
+            const { data } = await axios.get(`http://localhost:8080/account`)
             .then((response) => {
-                if(accountID){    
                     setView(response.data);
-                    console.log(view)}
+                    console.log(view)
             })
             setView(data)
 
@@ -34,7 +31,6 @@ export default function Sidebar() {
 
                 </div>
                 <div className='mt-4 flex flex-col gap-1 relative pt-[50px]'>
-                   {location.state.id}
                     <h1 className='text-white text-2xl text-center'>Admin Dashboard</h1>
                     <Link to='/news'><p className='border hover:bg-yellow-500 p-5'>News & Events</p></Link>
                     <Link to='/faculty'><p className='border hover:bg-yellow-500 p-5'>Teaching Staff's</p></Link>
